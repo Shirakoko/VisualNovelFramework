561ebb18a42b7c836499d18a79819540489f5b45
白雪姬 <97279549+Shirakoko@users.noreply.github.com>
2025-07-25 15:03:24 +0800

    调整UI，DialogItem高度自适应

--- Assets/Scripts/SingleDialogItem.cs
+++ Assets/Scripts/SingleDialogItem.cs
@@ -8,6 +8,11 @@ public class SingleDialogItem : MonoBehaviour
     [Header("存放对话内容的文字")]
     public Text contentText;
 
+    private void Start()
+    {
+        AdjustHeight();   
+    }
+
     public void SetSpeaker(string speaker)
     {
         if (speakerText != null)
@@ -22,5 +27,19 @@ public class SingleDialogItem : MonoBehaviour
         {
             contentText.text = content;
         }
+    }
+
+    public void AdjustHeight()
+    {
+        // 获取两个子控件的实际高度
+        float speakerHeight = speakerText.preferredHeight;
+        float contentHeight = contentText.preferredHeight;
+        
+        // 取最大高度作为自身高度
+        float maxHeight = Mathf.Max(speakerHeight, contentHeight);
+        
+        // 设置自身高度
+        RectTransform rt = GetComponent<RectTransform>();
+        rt.sizeDelta = new Vector2(rt.sizeDelta.x, maxHeight);
     }
 }
